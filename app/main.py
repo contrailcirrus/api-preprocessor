dd5b9f159ed6dc204116c7b3c038a9f313f8877a
Tristan Abbott <tristanhabbott@gmail.com>
2024-02-20 13:56:36 -0500

    wip: pycontrails stubs

--- app/main.py
+++ app/main.py
@@ -1,11 +1,40 @@
 """Entrypoint for the (internal) API hosting API Preprocessor methods."""
 
+import datetime
 import flask
+import numpy as np
+import xarray as xr
+import gcsfs
+
+from pycontrails import MetDataset
+from pycontrails.models.cocipgrid import CocipGrid
+from pycontrails.models.ps_model import PSGrid
+from pycontrails.models.humidity_scaling import ExponentialBoostLatitudeCorrectionHumidityScaling
+from pycontrails.physics import units
 
 from app.log import logger
 
 app = flask.Flask(__name__)
 
+# TODO: finalize values
+PROVISIONAL_COCIP_GRID_PARAMS = dict(
+    humidity_scaling=ExponentialBoostLatitudeCorrectionHumidityScaling(),
+    dt_integration= "5min",
+    max_age="12h",
+    met_slice_dt="1h",
+    target_split_size=100_000,
+    target_split_size_pre_SAC_boost=2.5,
+    max_altitude_m=None,
+    min_altitude_m=None,
+    azimuth=None,
+    segment_length=None,
+    dsn_dz_factor=0.665,
+    interpolation_use_indices=True,
+    interpolation_bounds_error=True,
+    show_progress=False,
+    filter_sac=True,
+    copy_source=True
+)
 
 @app.route("/", methods=["GET"])
 def health_check() -> tuple[str, int]:
@@ -13,6 +42,45 @@ def health_check() -> tuple[str, int]:
     logger.info("Call health check /")
     return "success", 200
 
+def _load_met_rad(t: datetime.datetime) -> tuple[MetDataset, MetDataset]:
+    bucket = "gs://contrails-301217-ecmwf-hres-forecast-v2-short-term"
+    forecast = t.strftime("%Y%m%d%H")
+    
+    pl = xr.open_zarr(f"{bucket}/{forecast}/pl.zarr/")
+    met = MetDataset(pl, provider="ECMWF", dataset="HRES", product="forecast")
+    variables = (v[0] if isinstance(v, tuple) else v for v in CocipGrid.met_variables)
+    met.standardize_variables(variables)
+    
+    sl = xr.open_zarr(f"{bucket}/{forecast}/sl.zarr/")
+    rad = MetDataset(sl, provider="ECMWF", dataset="HRES", product="forecast")
+    variables = (v[0] if isinstance(v, tuple) else v for v in CocipGrid.rad_variables)
+    rad.standardize_variables(variables)
+    return met, rad
+
+
+def _create_cocip_grid_source(t: datetime.datetime, flight_level: int) -> MetDataset:
+    hor_res = 0.25
+    dtype = np.float64
+    longitude = np.arange(-180, 180, hor_res, dtype=dtype)
+    latitude = np.arange(-80, 80.01, hor_res, dtype=dtype)
+    altitude_ft = flight_level * 100.0
+    level = units.ft_to_pl(altitude_ft)
+    return MetDataset.from_coords(
+        longitude=longitude,
+        latitude=latitude,
+        level=level,
+        time=t,
+    )
+
+def _create_cocip_grid_model(met: MetDataset, rad: MetDataset, aircraft_class: str) -> CocipGrid:
+    # TODO: set relevant parameters based on aircraft class
+    return CocipGrid(
+        met=met,
+        rad=rad,
+        aircraft_performance=PSGrid(),
+        **PROVISIONAL_COCIP_GRID_PARAMS
+    )
+    
 
 @app.route("/run", methods=["POST"])
 def run() -> tuple[str, int]:
@@ -36,10 +104,10 @@ def run() -> tuple[str, int]:
     # input params
     aircraft_class = "default"  # noqa:F841 please help to enum all of these
     flight_level = 300
-    model_run_at = 1707890400
-    model_predicted_at = 1707926400
+    model_run_at = 1708318800
+    model_predicted_at = 1708354800
     polygon_thresholds = [500000000, 5000000, 50000]
-    max_age_hr = 12  # noqa:F841
+    max_max_age_hr = 12  # noqa:F841
 
     # helpers
     offset_hrs = (model_predicted_at - model_run_at) // 60
@@ -59,6 +127,23 @@ def run() -> tuple[str, int]:
     ]
 
     # TODO: build cocip grid at 0.25deg x 0.25deg, export to gcs as netcdf file
+    run_at = datetime.datetime.fromtimestamp(model_run_at)
+    pred_at = datetime.datetime.fromtimestamp(model_predicted_at)
+    max_age = min(datetime.timedelta(hours=max_max_age_hr), run_at + datetime.timedelta(hours=72) - pred_at)
+
+    met, rad = _load_met_rad(run_at)
+    source = _create_cocip_grid_source(pred_at, flight_level)
+    model = _create_cocip_grid_model(met, rad, aircraft_class)
+
+    result = model.eval(source)
+    result.data.to_netcdf(...)
+
+    fs = gcsfs.GCSFileSystem()
+    with fs.open(grids_gcs_sink_path) as f:
+        result.data.to_netcdf(f)
+
+    # save w/ gcsfs
+
     # TODO: build polygon files for each threshold, export gcs as geojson
 
     logger.info("processing of job complete.  message id: foobar")
