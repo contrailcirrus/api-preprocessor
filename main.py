a99619a8b2c2fbb350ebd2ae9343bbfe0ae54e94
Nick Masson <nick.masson@breakthroughenergy.org>
2024-03-28 08:08:03 -0600

    Update/guarantee prediction future (#23)
    * skip over tardy jobs
    
    * reimplement siping
    
    * update readme
    
    * update exit to use sys pkg
    
    * update logic for half-step finite diff radiative flux calc stuff
    
    * tweak buffer logic
    
    ---------
    
    Co-authored-by: Tristan Abbott <tristanhabbott@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Entrypoint for the (internal) API hosting API Preprocessor methods."""
 
 import sys
+
 from lib.handlers import CocipHandler, JobSubscriptionHandler
 import lib.environment as env
 from lib.log import logger, format_traceback
@@ -28,6 +29,28 @@ def run():
         #    flight_level=300,
         #    aircraft_class="default",
         # )
+
+        # prune jobs where hres met data availability isn't sufficient
+        # -------
+        # The current (0.49.3) pycontrails implementation needs a buffer
+        # for differencing accumulated radiative fluxes.
+        # 1) job.model_predicted_at must be at least half an hour after job.model_run_at
+        # 2) prediction_runway_hrs must extend at least half an hour beyond CocipHandler.MAX_AGE_HR
+        prediction_wall = (
+            job.model_run_at + 72 * 60 * 60
+        )  # 72 hrs of fwd met data per model run
+        prediction_runway_hrs = (prediction_wall - job.model_predicted_at) / (60 * 60)
+        logger.info(
+            f"job should have {prediction_runway_hrs} of forward-looking hres data"
+        )
+        if (
+            job.model_predicted_at < job.model_run_at + 1800  # seconds in 0.5 hr
+            or prediction_runway_hrs < CocipHandler.MAX_AGE_HR + 0.5
+        ):
+            logger.info(f"skipping. not enough met data for job: {job}")
+            job_handler.ack()
+            return
+
         logger.info(f"generating outputs for job. job: {job}")
         cocip_handler = CocipHandler(
             env.SOURCE_PATH,
