3f3508433c023d8dbe1b98ec1efea3955ac4fa24
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-05-10 14:50:56 -0600

    2 dask workers

--- lib/handlers.py
+++ lib/handlers.py
@@ -35,7 +35,7 @@ from lib.log import format_traceback, logger
 from lib.schemas import ApiPreprocessorJob
 
 # oversubscribe dask workers
-pool = ThreadPoolExecutor(4)
+pool = ThreadPoolExecutor(2)
 dask.config.set(pool=pool)
 
 
