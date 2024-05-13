4e5779ad3f2b40ea8d2c0bfd7322d77db962c74c
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-05-13 09:35:38 -0600

    dask concurrency to 16

--- lib/handlers.py
+++ lib/handlers.py
@@ -35,7 +35,7 @@ from lib.log import format_traceback, logger
 from lib.schemas import ApiPreprocessorJob
 
 # oversubscribe dask workers
-pool = ThreadPoolExecutor(8)
+pool = ThreadPoolExecutor(16)
 dask.config.set(pool=pool)
 
 
