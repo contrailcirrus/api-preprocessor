21d8e1281fdc009045cd98e6866077aaab6b42c6
Nick Masson <nick.masson@breakthroughenergy.org>
2024-05-20 10:14:21 -0600

    add production GCP resources (#60)
    * add bq resources
    
    * bq prod resources to independent tf file
    
    * gcs prod resources
    
    * increase ttl on gcs bucket; dev
    
    * add pubsub; bq delivery

--- .cloud/cloud_storage_prod.tf
+++ .cloud/cloud_storage_prod.tf
@@ -2,13 +2,13 @@
 # Google Cloud Storage (GCS) resources
 # ------------------------------------
 
-resource "google_storage_bucket" "api_preprocessor_bucket_dev" {
+resource "google_storage_bucket" "api_preprocessor_bucket_prod" {
   default_event_based_hold    = false
   enable_object_retention     = false
   force_destroy               = false
   labels                      = {}
   location                    = "US-EAST1"
-  name                        = "contrails-301217-api-preprocessor-dev"
+  name                        = "contrails-301217-api-preprocessor-prod"
   project                     = "contrails-301217"
   public_access_prevention    = "enforced"
   requester_pays              = false
@@ -20,13 +20,5 @@ resource "google_storage_bucket" "api_preprocessor_bucket_dev" {
     read   = null
     update = null
   }
-  lifecycle_rule {
-  condition {
-    age = 30
-  }
-  action {
-    type = "Delete"
-  }
-}
 }
 
